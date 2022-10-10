439dfe0d1aadce59943cea251e16dfe9564d118a
Carlos Adrian Sanchez Monter <carlosadryan198915@gmail.com>
2022-10-10 10:29:39 -0500

    first version

---
+++ Validaciones.py
@@ -0,0 +1,39 @@
+class Validations:
+    def __init__(self):
+        self.saldo = 1000
+        self.movimientos = ["Saldo de 1000"]
+        self.contrasenia = "1235"
+
+    def validPass(self):
+        res = input("Ingresa contraseña: ")
+        return res == self.contrasenia
+
+    def validOption(self, entrada):
+        return not(entrada == "1" or entrada == "2" or entrada == "3")
+
+    def currentBalance(self):
+        print("Tu saldo es de {}".format(self.saldo))
+
+    def isAvailable(self, money):
+        try:
+            if int(money) < 1: raise Exception
+            valor = self.saldo - int(money)
+        except:
+            valor = -1
+        finally:
+            if valor < 0:
+                print("**No es posible ese valor**")
+                return False
+            else:
+                return True
+
+    def getMoney(self):
+        res =  input("Cuanto deceas retirar: \n")
+        if self.isAvailable(res):
+            self.saldo = self.saldo - int(res)
+            print("Retirando {}, te queda {}".format(res, self.saldo))
+            self.movimientos.append("Saldo Retirado de {}".format(res))
+
+    def history(self):
+        for x in range(0, len(self.movimientos)):
+            print("Movimiento {}. {}".format(x, self.movimientos[x]))
---
+++ main.py
@@ -0,0 +1,38 @@
+from Validaciones import Validations
+
+def printMenu():
+    print("\n" ,"- " *100)
+    return input("Menu: Enter\nSalir: q")
+
+def printOptions():
+    return input \
+        ("1. Consultar Saldo\n2. Retirar Saldo\n3. Hisorico de Movimientos \n")
+
+if __name__ == '__main__':
+    val = Validations()
+    password = False
+    for x in range(0, 3):
+        password = val.validPass()
+        if password == True: break
+        print("Contraseña erronea")
+    if password:
+        while True:
+            res = printMenu()
+            if res == "q": break
+            res = printOptions()
+            if val.validOption(res) :
+                print("Opcion no valida")
+                pass
+            elif res == "1":
+                val.currentBalance()
+            elif res == "2":
+                val.getMoney()
+            elif res == "3":
+                val.history()
+
+
+
+
+
+
+
